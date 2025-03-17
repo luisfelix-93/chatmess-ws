69076bf24b71d8319b44d63cc410fa54717868b0
luisfelix-93 <lfff6493@gmail.com>
2025-03-17 09:17:36 -0300

    20250317 - atualização de versão do TLS #8

--- src/services/MessageService.ts
+++ src/services/MessageService.ts
@@ -10,6 +10,7 @@ dotenv.config();
  */
 const httpsAgent = new https.Agent({
     rejectUnauthorized: false, // O agente vai passar a ignorar os erros de certificados
+    minVersion: 'TLSv1.2'
 })
 export default interface IMessageService {
     createMessage(pMessage: MessageDTO): Promise<Message | null>;
@@ -21,6 +22,11 @@ export default interface IMessageService {
 export default class MessageService implements IMessageService {
     constructor(){}
 
+    /**
+     * Método de envio de mensagem no chat
+     * @param pMessage : {MessageDTO}
+     * @returns 
+     */
     async createMessage(pMessage: MessageDTO): Promise<Message | null> {
         const body = JSON.stringify(pMessage);
         const config = {
@@ -33,7 +39,11 @@ export default class MessageService implements IMessageService {
         const response = await axios.request(config);
         return response.data.resultObject;
     }
-
+    /**
+     * Método que busca as mensagens da sala
+     * @param room : string
+     * @returns Message
+     */
     async findMessage(room: string): Promise<Message[] | null> {
         let message: Message[] = [];
         const url = process.env.URL_MESSAGE;
@@ -46,7 +56,11 @@ export default class MessageService implements IMessageService {
         message = response.data.resultObject;
         return message;
     }
-
+    /**
+     * Método que exclui a mensagem pelom id dela
+     * @param messageid 
+     * @returns 
+     */
     async deleteMessage(messageid: string): Promise<string | null> {
         const url = process.env.URL_MESSAGE;
         const config = {
@@ -57,7 +71,12 @@ export default class MessageService implements IMessageService {
         const response = await axios.request(config);
         return response.data.message;
     }
-
+    /**
+     * Método que atualiza a mensagem pelo ID dela
+     * @param id : string
+     * @param messageUpdate: MessageDTO
+     * @returns Message
+     */
     async updateMessage(id: string, messageUpdate: MessageDTO): Promise<object | null> {
         const body = JSON.stringify(messageUpdate);
         const url = process.env.URL_MESSAGE;
